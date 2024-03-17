e7a72a6a1f0e0fe7da0a77c699bca6ba38a3b150
Alicia M. Elliott <aliciae@alum.mit.edu>
2024-03-17 17:56:55 -0400

    Adds minor hdf5 parsing helper functions

--- pyvanadium/src/parsing/hdf5_reading.py
+++ pyvanadium/src/parsing/hdf5_reading.py
@@ -1,16 +1,53 @@
 import h5py as h5
 import hdf5_settings
 
-# Turn on object creation order tracking
-print(hdf5_settings.enable_object_creation_order_tracking())
-print(h5.get_config().track_order)
+
+def extract_group_names(group):
+    return list(group.keys())
 
 
-with h5.File("../../../database_design/test.h5", "r") as f:
-    print(f.keys())
+def print_tree(name, obj):
+    full_name = name.split("/")
+    # get offset spacing from the full name
+    offset = "    "
+    depth = len(full_name)
 
-    if "Raw data" in f.keys():
-        rd = f["Raw data"]
-        print(rd.keys())
+    if isinstance(obj, h5.Dataset):
+        # node is a dataset
+        print("   " + offset * (depth) + "└──[" + name.split("/")[-1] + "]")
+    else:
+        print("   " + offset * depth + name.split("/")[-1])
 
-    f.close()
+
+# └──
+def print_group_structure(grp):
+    print("")
+    print("├──" + grp.name.split("/")[-1])
+    grp.visititems(print_tree)
+
+
+def print_structure(grp):
+    # base = extract_group_names(grp)
+
+    for key, value in grp.items():
+        print("├── " + key)
+        value.visititems(print_tree)
+
+
+if __name__ == "__main__":
+    # general comment: we use `with` syntax to open hdf5 files so that we do
+    # not need to worry about explicitly closing the file at the end of our
+    # usage. So, this note is for anyone new to working with hdf5 files
+    # and/or h5py: if you open an h5 file using standard file open syntax,
+    # you must explicitly close the file to ensure the hdf5 file does not get
+    # corrupted by not being closed.
+    with h5.File("../../../database_design/test.h5", "r") as f:
+        names = extract_group_names(f)
+
+        # print_structure(f)
+        print_structure(f)
+        # f.visititems(print_tree)
+
+        if "Vanadium alloys" in f.keys():
+            valloys = f["Vanadium alloys"]
+            # print_group_structure(valloys)
--- pyvanadium/src/parsing/hdf5_writing.py
+++ pyvanadium/src/parsing/hdf5_writing.py
@@ -1,8 +1,2 @@
 import h5py as h5
 import hdf5_settings
-
-# Turn on creation order tracking, so that the resulting HDF file does not
-# get sorted alphanumerically. For end-user convenience, we want to have a
-# consistent organization of the first level groups that makes sense for easy
-# navigation.
-hdf5_settings.enable_object_creation_order_tracking()
